0b9c8923d4553587c4ea5b1d4250c2f392ad80ab
nikolaj <nikolajgliese@tutanota.com>
2022-08-07 10:39:51 +0200

    fixed output

--- tools/clang-tidy.sh
+++ tools/clang-tidy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Argument 1 is the path to the directory containing the compile_commands.json file
-BUILD_FOLDER=${1:-build}
+BUILD_FOLDER=target/build/
 OUTPUT_FOLDER=./docs/clang
 
 # bash script to apply clang-tidy to the entire codebase
 # we are excluding */build folder aswell as */ThirdPartAPI and */test as we do not want to make any changes to these files.
-find target -type d \( -path */Embedded_CPP_Library \) -prune -type f  -o -iname *.h -o -iname *.c -o -iname *.cpp -o -iname *.hpp\
- | xargs clang-tidy #-p $BUILD_FOLDER
+find target -not \( -path target/main/Components/esp32Cam_API -prune -o -path target/main/Components/image_conversions -prune -o -path target/build -prune \) \
+-name \*.h* -o -name \*.c* | xargs clang-tidy -p $BUILD_FOLDER -o $OUTPUT_FOLDER
 
