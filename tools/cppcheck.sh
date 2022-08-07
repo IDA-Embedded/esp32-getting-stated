f403363c3aa2165eded8ba79a51a1bdb612bdf0c
nikolaj <nikolajgliese@tutanota.com>
2022-08-07 10:26:56 +0200

    added complexity report

--- tools/cppcheck.sh
+++ tools/cppcheck.sh
@@ -5,5 +5,5 @@ OUTPUT_FOLDER=./docs/cppcheck
 
 # bash script to apply clang-tidy to the entire codebase
 # we are excluding */build folder aswell as */ThirdPartAPI and */test as we do not want to make any changes to these files.
-find target -type d \( -path */Embedded_CPP_Library -path */build \) -prune -type f  -o -iname *.h -o -iname *.c -o -iname *.cpp -o -iname *.hpp\
- | xargs cppcheck  --xml --xml-version=2 --enable=all --output-file=${OUTPUT_FOLDER}/cppcheck.xml
+find target -not \( -path target/main/Components/esp32Cam_API -prune -o -path target/main/Components/image_conversions -prune -o -path target/build -prune \) \
+-name \*.h* -o -name \*.c* | xargs cppcheck  --xml --xml-version=2 --enable=all --output-file=${OUTPUT_FOLDER}/cppcheck.xml
--- tools/lizard.sh
+++ tools/lizard.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Argument 1 is the path to the directory containing the compile_commands.json file
-BUILD_OUTPUT_FOLDER=./docs/complexity
+OUTPUT_FOLDER=./docs/complexity
 
 # bash script to apply clang-tidy to the entire codebase
 # we are excluding */build folder aswell as */ThirdPartAPI and */test as we do not want to make any changes to these files.
-find target -type d \( -path */Embedded_CPP_Library \) -prune -type f  -o -iname *.h -o -iname *.c -o -iname *.cpp -o -iname *.hpp\
- | xargs lizard  --html -o ${BUILD_OUTPUT_FOLDER}/complexity.html
+find target -not \( -path target/main/Components/esp32Cam_API -prune -o -path target/main/Components/image_conversions -prune -o -path target/build -prune \) \
+-name \*.h* -o -name \*.c* | xargs lizard  --html -o ${BUILD_OUTPUT_FOLDER}/complexity.html
