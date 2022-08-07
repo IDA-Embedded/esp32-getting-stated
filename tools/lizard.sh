445b955845e4e7acc42a46b3b80d4daeeb93dbdd
nikolaj <nikolajgliese@tutanota.com>
2022-08-07 10:43:23 +0200

    fixed output

--- tools/lizard.sh
+++ tools/lizard.sh
@@ -4,5 +4,5 @@ OUTPUT_FOLDER=./docs/complexity
 
 # bash script to apply clang-tidy to the entire codebase
 # we are excluding */build folder aswell as */ThirdPartAPI and */test as we do not want to make any changes to these files.
-find target -not \( -path target/main/Components/esp32Cam_API -prune -o -path target/main/Components/image_conversions -prune -o -path target/build -prune \) \
+find . -not \( -path ./target/main/Components/esp32Cam_API -prune -o -path ./target/main/Components/image_conversions -prune -o -path ./target/build -prune \) \
 -name \*.h* -o -name \*.c* | xargs lizard  #--html -o ${OUTPUT_FOLDER}/complexity.html
